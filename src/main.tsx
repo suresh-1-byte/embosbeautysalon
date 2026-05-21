24ea3cb8757081d1ad24ed5107f881a37eadf90a
Suresh Kathirvel <sureshkathirvel601@gmail.com>
2026-05-21 13:27:52 +0530

    fix: remove duplicate createRoot call in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -22,9 +22,3 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>
 );
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
